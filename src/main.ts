387ab5f25fb83409832b1f0f6d7b9fc6abb661fd
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-07 18:48:34 +0000

    add pension and net salary output fields; move `salary.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
-import { Salary, createEmptySalary } from "./types/salary.js";
+import { Salary, createEmptySalary } from "./salary.js";
 
 const grossInput = document.getElementById("gross-input") as HTMLInputElement | null;
 const grossOutput = document.getElementById("gross-output") as HTMLParagraphElement | null;
+const pensionInput = document.getElementById("pension-input") as HTMLInputElement | null;
+const pensionOutput = document.getElementById("pension-output") as HTMLParagraphElement | null;
+const netOutput = document.getElementById("net-output") as HTMLParagraphElement | null;
 
 grossInput?.addEventListener("keyup", (e) => {
   let salary = createNewSalary();
-  updateGrossMonhtly(salary);
+  updateOutputFields(salary);
 });
 
-function updateGrossMonhtly(salary: Salary) {
-  if (grossOutput == null) return;
-  grossOutput.textContent = `£${salary.gross.toFixed(2)}`;
+pensionInput?.addEventListener("keyup", (e) => {
+  let salary = createNewSalary();
+  updateOutputFields(salary);
+});
+
+function updateOutputFields(salary: Salary) {
+  if (grossOutput != null) {
+    grossOutput.textContent = `£${salary.gross.toFixed(2)}`;
+  }
+  if (pensionOutput != null) {
+    pensionOutput.textContent = `£${salary.pension.toFixed(2)}`;
+  }
+  if (netOutput != null) {
+    netOutput.textContent = `£${salary.net.toFixed(2)}`;
+  }
 }
 
 function createNewSalary(): Salary {
@@ -22,6 +37,14 @@ function createNewSalary(): Salary {
       salary.gross = 0;
     }
   }
+  if (pensionInput != null) {
+    salary.pension = salary.gross * 0.01 * Number(pensionInput.value);
+    if (isNaN(salary.pension)) {
+      salary.pension = 0;
+    }
+  }
+
+  salary.net = salary.gross - salary.pension;
 
   return salary;
 }
