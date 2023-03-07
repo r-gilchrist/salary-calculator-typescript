be1fa8e519239af37b867e6db702742f992699a1
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-07 19:11:13 +0000

    Add initial (AKA full of bugs) version of all field calculations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { Salary, createEmptySalary } from "./salary.js";
 
 const grossInput = document.getElementById("gross-input") as HTMLInputElement | null;
-const grossOutput = document.getElementById("gross-output") as HTMLParagraphElement | null;
 const pensionInput = document.getElementById("pension-input") as HTMLInputElement | null;
+
+const grossOutput = document.getElementById("gross-output") as HTMLParagraphElement | null;
+const incomeTaxOutput = document.getElementById("tax-output") as HTMLParagraphElement | null;
+const niOutput = document.getElementById("NI-output") as HTMLParagraphElement | null;
+const studentOutput = document.getElementById("student-output") as HTMLParagraphElement | null;
 const pensionOutput = document.getElementById("pension-output") as HTMLParagraphElement | null;
 const netOutput = document.getElementById("net-output") as HTMLParagraphElement | null;
 
@@ -18,21 +22,32 @@ pensionInput?.addEventListener("keyup", (e) => {
 
 function updateOutputFields(salary: Salary) {
   updateOutputField(grossOutput, salary.gross);
+  updateOutputField(incomeTaxOutput, salary.income_tax);
+  updateOutputField(niOutput, salary.nat_insurance);
+  updateOutputField(studentOutput, salary.student_loan);
   updateOutputField(pensionOutput, salary.pension);
   updateOutputField(netOutput, salary.net);
 }
 
-function updateOutputField(element: HTMLParagraphElement | null, amount: Number) {
+function updateOutputField(element: HTMLParagraphElement | null, amount: number) {
   if (element == null) return;
-  element.textContent = `£${amount.toFixed(2)}`;
+  element.textContent = `£${(amount / 12).toFixed(2)}`;
 }
 
 function getSalary(): Salary {
   let salary = createEmptySalary();
 
-  salary.gross = getInputAmount(grossInput) / 12;
+  salary.gross = getInputAmount(grossInput);
   salary.pension = salary.gross * 0.01 * getInputAmount(pensionInput);
-  salary.net = salary.gross - salary.pension;
+
+  let adjusted_gross = salary.gross - salary.pension;
+
+  salary.income_tax = (adjusted_gross - 12500) * 0.2;
+  salary.nat_insurance = (adjusted_gross - 12500) * 0.12;
+  salary.student_loan = (adjusted_gross - 20195) * 0.09;
+
+  salary.net =
+    salary.gross - salary.income_tax - salary.nat_insurance - salary.student_loan - salary.pension;
 
   return salary;
 }
