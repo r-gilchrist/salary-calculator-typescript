3783f740384ebe4caef33a8b8cb100f80471d286
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-07 18:05:34 +0000

    add annual to monthly gross conversion

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+const grossInput = document.getElementById("gross-input") as HTMLInputElement | null;
+const grossOutput = document.getElementById("gross-output") as HTMLParagraphElement | null;
+
+console.log(grossInput);
+
+grossInput?.addEventListener("keyup", (e) => {
+  updateGrossMonhtly();
+});
+
+function updateGrossMonhtly() {
+  if (grossInput == null || grossOutput == null) return;
+  let inputAmount = Number(grossInput.value);
+  if (isNaN(inputAmount)) {
+    inputAmount = 0;
+  }
+  console.log(grossInput);
+
+  let monthlyGross = inputAmount / 12;
+
+  grossOutput.textContent = `£${monthlyGross.toFixed(2)}`;
+}
+
+export {};
