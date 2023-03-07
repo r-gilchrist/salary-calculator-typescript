0be6adedf1955afdaf4f966adb3aa4eee8a4dce8
Ryan Gilchrist <56300878+r-gilchrist@users.noreply.github.com>
2023-03-07 22:47:45 +0000

    add support for salaries up to £100k

--- src/salary.ts
+++ src/salary.ts
@@ -7,6 +7,18 @@ export type Salary = {
   net: number;
 };
 
+type Tax = {
+  basic_rate: number;
+  basic_threshold: number;
+  higher_rate: number;
+  higher_threshold: number;
+};
+
+type StudentLoan = {
+  rate: number;
+  threshold: number;
+};
+
 export function createEmptySalary(): Salary {
   return {
     gross: 0,
@@ -20,14 +32,43 @@ export function createEmptySalary(): Salary {
 
 export function populateTaxes(salary: Salary): Salary {
   let after_sacrifice = salary.gross - salary.pension;
-  salary.income_tax = calculateTax(after_sacrifice, 12570, 20);
-  salary.nat_insurance = calculateTax(after_sacrifice, 12570, 12);
-  salary.student_loan = 12 * Math.floor(calculateTax(after_sacrifice, 20195, 9) / 12);
+
+  salary.income_tax = calculateTax(after_sacrifice, {
+    basic_rate: 20,
+    basic_threshold: 12570,
+    higher_rate: 40,
+    higher_threshold: 50270,
+  });
+
+  salary.nat_insurance = calculateTax(after_sacrifice, {
+    basic_rate: 12,
+    basic_threshold: 12570,
+    higher_rate: 2,
+    higher_threshold: 50270,
+  });
+
+  salary.student_loan = calculateStudentLoan(after_sacrifice, {
+    rate: 9,
+    threshold: 20195,
+  });
+
   salary.net = after_sacrifice - salary.income_tax - salary.nat_insurance - salary.student_loan;
+
   return salary;
 }
 
-function calculateTax(amount: number, threshold: number, percentage: number): number {
-  if (amount < threshold) return 0;
-  return 0.01 * percentage * (amount - threshold);
+function calculateTax(amount: number, tax: Tax): number {
+  let total: number = 0;
+  if (amount < tax.basic_threshold) return total;
+  total += (amount - tax.basic_threshold) * 0.01 * tax.basic_rate;
+  console.log("Lower is", total);
+  if (amount < tax.higher_threshold) return total;
+  total += 0.01 * (amount - tax.higher_threshold) * (tax.higher_rate - tax.basic_rate);
+  console.log("Higher is", total);
+  return total;
+}
+
+function calculateStudentLoan(amount: number, student_loan: StudentLoan): number {
+  if (amount < student_loan.threshold) return 0;
+  return 12 * Math.floor((0.01 * (amount - student_loan.threshold) * student_loan.rate) / 12);
 }
